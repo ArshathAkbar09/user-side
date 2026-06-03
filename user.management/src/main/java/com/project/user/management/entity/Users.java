ad1410cc258489573ba9c5322ab9069a2d0fb179
ArshathAkbar09 <8sz86sbfgy@privaterelay.appleid.com>
2026-06-03 15:57:55 +0530

    commit 2

--- user.management/src/main/java/com/project/user/management/entity/Users.java
+++ user.management/src/main/java/com/project/user/management/entity/Users.java
@@ -1,5 +1,141 @@
 package com.project.user.management.entity;
 
-public class Users {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, unique = true, length = 50)
+	private String name;
+	
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false, unique = true, length = 25)
+	private String mobile;
+	
+	@Column(nullable = false)
+	private localDate dob;
+	
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+	
+	@Column(nullable = false, length = 200)
+	private String address;
+	
+	@Column(length = 1000)
+	private String url;
+
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Users(long id, String name, String email, String password, String mobile, localDate dob, Gender gender,
+			String address, String url) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.mobile = mobile;
+		this.dob = dob;
+		this.gender = gender;
+		this.address = address;
+		this.url = url;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public localDate getDob() {
+		return dob;
+	}
+
+	public void setDob(localDate dob) {
+		this.dob = dob;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", mobile="
+				+ mobile + ", gender=" + gender + ", address=" + address + ", url=" + url + "]";
+	}
+	
+	
+	
 }
